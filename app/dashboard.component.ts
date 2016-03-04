76cbe59fb3dbe248f6a1ff24a489669b09896da2
Piotr Ludwiczuk <piotr.ludwiczuk@outbox.pl>
2016-03-04 13:00:18 +0100

    Top heroes

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,9 +1,27 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {HeroService} from './services/hero.service';
 
 @Component({
     selector: 'dashboard',
-    template: '<h3>My dashboard</h3>'
+    templateUrl: 'app/templates/dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
     
+    heroes: Hero[] = [];
+    
+    constructor(private _heroService: HeroService) {
+        
+    }
+    
+    ngOnInit() {
+        console.log('Dashboard init');
+        this._heroService.getHeroes()
+            .then(heroes => {
+                this.heroes = heroes.slice(1, 5);
+            });
+    }
+    
+    gotoDetail() {
+        /* not implemented yet */
+    }
 }
--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -4,6 +4,7 @@ import {HEROES} from '../mock-heroes';
 @Injectable()
 export class HeroService {
     getHeroes() {
+        
         return Promise.resolve(HEROES);
     }
     
