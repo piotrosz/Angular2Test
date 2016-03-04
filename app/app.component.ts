65fa1826ed5a9b5420ffb5e524ac4c51be313cd1
Piotr Ludwiczuk <piotr.ludwiczuk@outbox.pl>
2016-03-04 12:26:04 +0100

    Add routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,27 @@
 import {Component} from 'angular2/core';
-import {HeroDetailComponent} from './hero-detail.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
 import {HeroService} from './services/hero.service';
-import {OnInit} from 'angular2/core';
+import {HeroesComponent} from './heroes.component';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: 'app/templates/heroes.html',
-    styleUrls: ["app/content/css/hero.css"],
-    directives: [HeroDetailComponent],
-    providers: [HeroService]
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
-export class AppComponent implements OnInit {
-    
-    constructor(private _heroService: HeroService) {
+
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
     }
-    
-    ngOnInit() {
-        this.getHeroes();
-    }
-    
-    public title = 'Tour of Heroes';
-    public selectedHero: Hero = {
-      id: 1,
-      name: 'Windstorm'  
-    };
-    
-    getHeroes() {
-        this._heroService
-            .getHeroes()
-            .then(heroes => this.heroes = heroes);
-    }
-    
-    public heroes: Hero[];
-    
-    onSelect(hero: Hero) {
-        this.selectedHero = hero;
-    }
+])
+export class AppComponent {
+    title = 'Tour of heroes';
 }
---
+++ app/heroes.component.ts
@@ -0,0 +1,38 @@
+import {Component} from 'angular2/core';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './services/hero.service';
+import {OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'heroes',
+    templateUrl: 'app/templates/heroes.html',
+    styleUrls: ["app/content/css/hero.css"],
+    directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+    
+    constructor(private _heroService: HeroService) {
+    }
+    
+    ngOnInit() {
+        this.getHeroes();
+    }
+    
+    public title = 'Tour of Heroes';
+    public selectedHero: Hero = {
+      id: 1,
+      name: 'Windstorm'  
+    };
+    
+    getHeroes() {
+        this._heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes);
+    }
+    
+    public heroes: Hero[];
+    
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+}
